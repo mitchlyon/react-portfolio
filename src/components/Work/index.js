5590744683d7837ea6875a913464398fa1f02825
Mitchell Lyon <kekoa.lyon@gmail.com>
2021-12-19 10:49:33 -0700

    done

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -15,37 +15,37 @@ function Work() {
                 </div>
             </a>
 
-            <a href="https://mitchlyon.github.io/run-buddy/" class="project">
+            <a href="https://joseepina.github.io/bc-project-01/" alt="Spotify API" class="project">
                 <div class="project-2">
-                    <h3>Future</h3>
-                    <h4>project</h4>
+                    <h3>Spotify API</h3>
+                    <h4>Front end</h4>
 
                 </div>
             </a>
 
-            <a href="https://mitchlyon.github.io/run-buddy/" class="project">
+            <a href="https://github.com/andydhpkp/pass-stache" alt="Pass Stache" class="project">
                 <div class="project-3">
-                    <h3>Future</h3>
-                    <h4>project</h4>
+                    <h3>Pass Stache</h3>
+                    <h4>Full Stack Application</h4>
 
                 </div>
             </a>
 
-            <a href="https://mitchlyon.github.io/run-buddy/" class="project">
+            <a href="https://mitchlyon.github.io/weather-dashboard/" alt="Weather Dashboard" class="project">
                 <div class="project-4">
-                    <h3>Future</h3>
-                    <h4>project</h4>
+                    <h3>Weather Dashboard</h3>
+                    <h4>API</h4>
 
                 </div>
             </a>
 
-            <a href="https://mitchlyon.github.io/run-buddy/" class="project">
+            {/* <a href="https://mitchlyon.github.io/run-buddy/" class="project">
                 <div class="project-5">
                     <h3>Future</h3>
                     <h4>project</h4>
 
                 </div>
-            </a>
+            </a> */}
 
         </section>
     );
